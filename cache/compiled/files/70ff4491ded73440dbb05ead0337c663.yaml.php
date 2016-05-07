f3e946a857575bdb22085dccd8654b6b5253f234
elporfirio <elporfirio@gmail.com>
2016-05-07 12:29:25 -0500

    se agrega gitignore

--- cache/compiled/files/70ff4491ded73440dbb05ead0337c663.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/elporfir/public_html/music/user/config/media.yaml',
-    'modified' => 1462596973,
-    'data' => [
-        
-    ]
-];
